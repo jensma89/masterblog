dfd36003b040824164513a0c0b37b936d853069f
Jens M. <jensmayer89@gmail.com>
2025-08-22 10:38:32 +0200

    Make some POST handling to storage

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
 import json as js
+from flask import (Flask,
+                   render_template,
+                   request, redirect,
+                   url_for)
 
 app = Flask(__name__)
 
@@ -16,8 +19,27 @@ def index():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        pass
+        new_post = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "content": request.form["content"]
+        }
+
+        try:
+            with open("blog_storage.json", "r") as file:
+                blog_posts = js.load(file)
+        except FileNotFoundError:
+            blog_posts = []
+
+        blog_posts.append(new_post)
+
+        with open("blog_storage.json", "w") as file:
+            js.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for("index"))
     return render_template("add.html")
+
+
 
 
 
