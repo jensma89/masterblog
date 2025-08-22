c8136560246b6f2696cb8edfcc265ae2104701b4
Jens M. <jensmayer89@gmail.com>
2025-08-22 15:20:59 +0200

    Add like & dislike feature

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ def add():
             "id": len(blog_posts) + 1,
             "title": request.form.get("title", "Untitled"),
             "author": request.form.get("author", "Anonymous"),
-            "content": request.form.get("content", "")
+            "content": request.form.get("content", ""),
+            "likes": 0,
+            "dislikes": 0
         }
 
         blog_posts.append(new_post)
@@ -67,6 +69,7 @@ def update(post_id):
         with open("blog_storage.json", "r") as file:
             blog_posts = js.load(file)
     except FileNotFoundError:
+        print("File not found")
         blog_posts = []
 
     post = next((p for p in blog_posts if p["id"] == post_id), None)
@@ -86,6 +89,44 @@ def update(post_id):
     return render_template("update.html", post=post)
 
 
+@app.route("/like/<int:post_id>", methods=["POST"])
+def like(post_id):
+    try:
+        with open("blog_storage.json", "r") as file:
+            blog_posts = js.load(file)
+    except FileNotFoundError:
+        print("File not found")
+        blog_posts = []
+
+    for post in blog_posts:
+        if post["id"] == post_id:
+            post["likes"] += 1
+            break
+
+    with open("blog_storage.json", "w") as file:
+        js.dump(blog_posts, file, indent=4)
+
+    return redirect(url_for("index"))
+
+
+@app.route("/dislike/<int:post_id>", methods=["POST"])
+def dislike(post_id):
+    try:
+        with open("blog_storage.json", "r") as file:
+            blog_posts = js.load(file)
+    except FileNotFoundError:
+        print("File not found")
+        blog_posts = []
+
+    for post in blog_posts:
+        if post["id"] == post_id:
+            post["dislikes"] += 1
+            break
+
+    with open("blog_storage.json", "w") as file:
+        js.dump(blog_posts, file, indent=4)
+
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
