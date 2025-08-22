9da16967381dce7a151b22a7fa04dc7daedbf41a
Jens M. <jensmayer89@gmail.com>
2025-08-22 10:23:10 +0200

    Add the add template and route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json as js
 
 app = Flask(__name__)
@@ -9,7 +9,19 @@ def index():
     with open("blog_storage.json", "r") as file:
         blog_posts = js.load(file)
 
-    return render_template("index.html", posts=blog_posts)
+    return render_template("index.html",
+                           posts=blog_posts)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        pass
+    return render_template("add.html")
+
+
+
+
 
 
 if __name__ == "__main__":
